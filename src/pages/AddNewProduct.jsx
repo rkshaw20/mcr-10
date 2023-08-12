1fc1156db95cad88861abafbe439ef77b1db4951
rkshaw20 <rkshaw2026@gmail.com>
2023-08-12 22:16:42 +0530

    product

--- src/pages/AddNewProduct.jsx
+++ src/pages/AddNewProduct.jsx
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Form } from 'react-router-dom';
+import { useDataContext } from '../context/DataContextProvider';
 
 // id: 4,
 //       department: 'Clothing',
@@ -25,7 +26,6 @@ import { Form } from 'react-router-dom';
 //         'https://images.meesho.com/images/products/7488175/5b8f3_512.webp',
 const AddNewProduct = () => {
   const initialValue = {
-    id: '',
     department: '',
     name: '',
     description: '',
@@ -37,21 +37,24 @@ const AddNewProduct = () => {
     imageURL: '',
   };
   const [inputValue, setInputValue] = useState(initialValue);
+  const {dispatch}=useDataContext()
 
   const handleInput = event => {
     setInputValue({ ...inputValue, [event.target.name]: event.target.value });
   };
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(inputValue);
+    dispatch({type:'ADD_NEW_PRODUCT',payload:inputValue})
+    
+    setInputValue(initialValue)
   };
 
   return (
-    <Flex>
+    <Flex flexDirection='column' >
       <Flex>
         <Heading>Add New Product</Heading>
       </Flex>
-      <Flex flexDir="column">
+      <Flex flexDir="column" h='full' >
         <form onSubmit={handleSubmit}>
           <FormControl>
             <FormLabel>Department</FormLabel>
@@ -59,7 +62,7 @@ const AddNewProduct = () => {
               width="100px"
               name="department"
               id="department"
-        
+        isRequired
               onChange={handleInput}
             >
               <option value="Kitchen">Kitchen</option>
@@ -68,39 +71,39 @@ const AddNewProduct = () => {
             </Select>
           </FormControl>
           <FormControl><FormLabel>Name:</FormLabel>
-          <Input onChange={handleInput} name='name' /></FormControl>
+          <Input isRequired onChange={handleInput} name='name' value={inputValue.name} /></FormControl>
           <FormControl>
             <FormLabel>Descirption</FormLabel>
-            <Textarea name='descrition' onChange={handleInput} />
+            <Textarea isRequired name='description' onChange={handleInput} value={inputValue.description} />
           </FormControl>
           <FormControl>
             <FormLabel>Price</FormLabel>
-            <Input type='number' name="price" onClick={handleInput} />
+            <Input isRequired type='number' name="price" onChange={handleInput} value={inputValue.price} />
           </FormControl>
           <FormControl>
             <FormLabel>stock</FormLabel>
-            <Input type='number' name="stock" onClick={handleInput} />
+            <Input isRequired type='number' name="stock" onChange={handleInput} value={inputValue.stock}/>
           </FormControl>
           <FormControl>
             <FormLabel>SKU</FormLabel>
-            <Input  name="sku" onClick={handleInput} />
+            <Input isRequired name="sku" onChange={handleInput} value={inputValue.sku} />
           </FormControl>
           <FormControl>
 
             <FormLabel>Supplier</FormLabel>
-            <Input  name="supplier" onClick={handleInput} />
+            <Input isRequired name="supplier" onChange={handleInput} value={inputValue.supplier} />
           </FormControl>
           <FormControl>
 
             <FormLabel>Delivered</FormLabel>
-            <Input type='number' name="delivered" onClick={handleInput} />
+            <Input isRequired type='number' name="delivered" onChange={handleInput} value={inputValue.delivered} />
           </FormControl>
           <FormControl>
 
             <FormLabel>Image URL</FormLabel>
-            <Input name="imageURL" onClick={handleInput} />
+            <Input isRequired name="imageURL" onChange={handleInput} value={inputValue.imageURL} />
           </FormControl>
-          <Button>Add Product</Button>
+          <Button type='submit' m={2} bgColor='blue.400' >Add Product</Button>
         </form>
       </Flex>
     </Flex>
--- src/reducer/DataReducer.js
+++ src/reducer/DataReducer.js
@@ -1,5 +1,7 @@
-import { json } from "react-router-dom";
+import {v4 as uuid} from 'uuid'
 import {  inventoryData } from "../utils/constant";
+
+const ADD_NEW_PRODUCT = 'ADD_NEW_PRODUCT';
 
 const setInventoryData=localStorage.setItem('inventory data', JSON.stringify(inventoryData) );
 const localInventoryData=localStorage.getItem('inventory data');
@@ -8,9 +10,24 @@ export const dataInitialState = {
     data:JSON.parse(localInventoryData),
   };
 
-  export const DataReducer=(state,action)=>{
-    switch(action.type){
 
-    }
+
+  export const DataReducer = (state, action) => {
+    switch (action.type) {
+      case ADD_NEW_PRODUCT:
+          const newProduct = {
+              id: uuid(),
+              ...action.payload
+          };
+          const updatedData = [...state.data, newProduct];
+
+          localStorage.setItem('inventory data', JSON.stringify(updatedData));
+          return {
+              ...state,
+              data: updatedData,
+          };
+
+      default:
+          return state;
   }
-  
+};
